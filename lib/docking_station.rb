6214cf585b62353d8042f76437eae62146b8d597
Avril Harding <aviharding@gmail.com>
2020-02-18 16:07:45 +0000

    defined a method using a attr_reader

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,12 @@
 class DockingStation
+
+attr_reader :bike
+
   def release_bike
     release_bike = Bike.new
   end
 
   def dock_bike(bike)
   @bike =  bike
-  end
-
-  def bike
-    @bike
   end
 end
