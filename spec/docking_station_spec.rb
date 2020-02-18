7b49a5620c2306bb9c1220301b900cfc3faf70e5
Avril Harding <aviharding@gmail.com>
2020-02-18 14:31:43 +0000

    wrote a failing test for docking a bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,7 +12,14 @@ describe DockingStation do
     it "new bike works" do
       expect(bike.working?).to eq(true)
     end
+
+    bike = docking_station.dock_bike
+    it "it docks the bike at the docking station" do
+     expect(bike).to respond_to(DockingStation)
+   end
+
 end
+
   # docking_station = DockingStation.new
   # it "can release a bike" do
   #   expect(docking_station).to respond_to(:release_bike)
